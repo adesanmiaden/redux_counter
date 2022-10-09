2646e16e7044ec5fda6513b502bf63e7b6a17a62
Adesanmi Adeniyi <adesanmi.adeniyi@tellerium.io>
2022-10-08 20:32:48 -0700

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { counterActions } from '../store';
+import classes from './Counter.module.css';
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
+
+  const incrementHandler = () => {
+    dispatch (counterActions.increment())
+  }
+
+   const decrementHandler = () => {
+    dispatch (counterActions.decrement())
+  }
+
+  const toggleCounterHandler = () => {
+    dispatch (counterActions.toggle())
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+      <button onClick={incrementHandler}>Increase</button>
+      <button onClick={decrementHandler}>Decrease</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;

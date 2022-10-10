4b1cd737920de1c89d3604804f56a5361251518e
Adesanmi Adeniyi <adesanmi.adeniyi@tellerium.io>
2022-10-09 22:22:07 -0700

    redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
 import { createStore } from 'redux';
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
+const initialCounterState = {
     counter: 0,
     showCounter: true
 }
 
 const counterSlice = createSlice({
     name: 'counter',
-    initialState,
+    initialState: initialCounterState,
     reducers: {
     increment(state){
         state.counter++
@@ -23,6 +23,23 @@ const counterSlice = createSlice({
         state.showCounter = !state.showCounter
     }
 }
+})
+
+const initialAuthState = {
+    isAuthenticated: false
+}
+
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: initialAuthState,
+    reducers: {
+        login(state){
+            state.isAuthenticated = true
+        },
+        logout(state){
+            state.isAuthenticated = false
+        }
+    }
 })
 
 // const counterReducer = (state = initialState, action) =>{
@@ -47,9 +64,13 @@ const counterSlice = createSlice({
 // }
 
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: {
+        counter: counterSlice.reducer,
+        auth: authSlice.reducer
+    }
 });
 
-export const counterActions = counterSlice.actions
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
